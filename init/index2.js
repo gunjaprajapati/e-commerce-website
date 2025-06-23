73ae47475815b4dec88865a6bd22974955b2e492
Gunja Prajapati <gunjaprajapati23@gmail.com>
2025-06-23 09:43:38 +0530

    Updated code with final fixes and MongoDB Atlas setup

--- init/index2.js
+++ init/index2.js
@@ -6,7 +6,7 @@ const Category = require("../models/category.js");
 
 
 //(1.1) creating Wonderlust database
-const mongo_url = "mongodb://127.0.0.1:27017/ecommerce_db";
+const mongo_url = "mongodb+srv://ecom_user:yourpassword123@cluster0.ikum9.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0";
 
 //(1.2) calling main function
 main()
